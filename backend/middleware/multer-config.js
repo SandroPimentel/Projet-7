e1c1cecc374686a1324ab250a3e2e95261c799b4
SandroPimentel <130643138+SandroPimentel@users.noreply.github.com>
2024-01-02 12:53:44 +0100

    Conversion des images au format webp

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,4 +1,7 @@
 const multer = require('multer');
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
 
 const MIME_TYPES = {
     'image/jpg': 'jpg',
@@ -17,4 +20,52 @@ const storage = multer.diskStorage({
     }
 });
 
-module.exports = multer({storage: storage}).single('image');
+const fileFilter = (req, file, callback) => {
+    if (MIME_TYPES[file.mimetype]) {
+        callback(null, true);
+    } else {
+        callback(null, false);
+    }
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter }).single('image');
+
+module.exports = (req, res, next) => {
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            return res.status(500).json({ error: err.message });
+        } else if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+
+        if (req.file) {
+            const fullPath = path.join('images', req.file.filename);
+            sharp(fullPath)
+                .toFormat('webp')
+                .webp({ quality: 100 })
+                .toBuffer()
+                .then(data => {
+                    fs.writeFile(fullPath.replace(/\.[^/.]+$/, "") + ".webp", data, (err) => {
+                        if (err) {
+                            return res.status(500).json({ error: err.message });
+                        }
+
+                        req.file.filename = req.file.filename.replace(/\.[^/.]+$/, "") + ".webp";
+                        req.file.mimetype = 'image/webp';
+                        req.file.path = fullPath.replace(/\.[^/.]+$/, "") + ".webp";
+                        fs.unlink(fullPath, (err) => {
+                            if (err) {
+                                console.error(`Error deleting original file: ${err}`);
+                            }
+                            next();
+                        });
+                    });
+                })
+                .catch(err => {
+                    return res.status(500).json({ error: err.message });
+                });
+        } else {
+            next();
+        }
+    });
+};
